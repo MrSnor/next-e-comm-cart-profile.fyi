6708e93c40b93d7b1e06834590659b50d33360b5
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2024-08-17 13:29:46 +0530

    feat: add feature to add discount codes

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,10 +1,10 @@
 import { useCart } from "../contexts/CartContext";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Product } from "../types";
 import CartItem from "@/components/CartItem";
 import CountUp from "react-countup";
-import { Toaster } from "sonner";
+import { Toaster, toast } from "sonner";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -16,13 +16,111 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Transition } from "@headlessui/react";
 import { cn } from "@/lib/utils";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/16/solid";
+
+// constants for discount types
+const DISCOUNT_TYPES = {
+  PERCENTAGE: "percentage",
+  FLAT: "flat",
+};
+
+const discountData = [
+  {
+    code: "FIRST10",
+    discountType: DISCOUNT_TYPES.PERCENTAGE,
+    discount: 50,
+  },
+  {
+    code: "OFF20",
+    discountType: DISCOUNT_TYPES.PERCENTAGE,
+    discount: 20,
+  },
+  {
+    code: "HBD26",
+    discountType: DISCOUNT_TYPES.FLAT,
+    discount: 1000,
+  },
+];
 
 export default function Cart() {
   const { cart, updateQuantity, removeItem, clearCart } = useCart();
   const [products, setProducts] = useState<Product[]>([]);
+  const [discountCode, setDiscountCode] = useState("");
+  const [isDiscountCodeValid, setIsDiscountCodeValid] = useState<
+    boolean | null
+  >(null);
+  const [cartSummary, setCartSummary] = useState({
+    subTotal: 0,
+    discount: 0,
+    total: 0,
+  });
 
+  const calculateSubtotal = (): number => {
+    return products.reduce((total, product) => {
+      return total + product.price * (cart[product.id] || 0);
+    }, 0);
+  };
+
+  const applyDiscount = (subTotal: number, disCode: string = ""): number => {
+    const discObj = discountData.find((item) => item.code === disCode);
+    let discount = 0;
+
+    if (discObj) {
+      switch (discObj.discountType) {
+        case DISCOUNT_TYPES.PERCENTAGE:
+          discount = subTotal * (discObj.discount / 100);
+          toast.success(`Discount applied: ${discObj.discount}%`);
+          break;
+        case DISCOUNT_TYPES.FLAT:
+          discount = discObj.discount;
+          break;
+        default:
+          discount = 0;
+          break;
+      }
+    }
+
+    return discount;
+  };
+
+  const updateCartSummary = (disCode: string = "") => {
+    const subTotal = calculateSubtotal();
+    const discount = applyDiscount(subTotal, disCode);
+    const total = subTotal - discount;
+
+    setCartSummary({ subTotal, discount, total });
+  };
+
+  // handle discount code updates
+  const handleDiscountCodeChange = (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === "Enter") {
+      const code = e.currentTarget.value.toUpperCase();
+
+      const discObj = discountData.find((item) => item.code === code);
+      switch (discObj?.discountType) {
+        case DISCOUNT_TYPES.PERCENTAGE:
+          toast.success(`Discount applied: ${discObj.discount}%`);
+          setIsDiscountCodeValid(true);
+          break;
+        case DISCOUNT_TYPES.FLAT:
+          toast.success(`Discount applied: $${discObj.discount}`);
+          setIsDiscountCodeValid(true);
+          break;
+        default:
+          toast.warning("Invalid discount code");
+          setIsDiscountCodeValid(false);
+          break;
+      }
+
+      setDiscountCode(code);
+    }
+  };
+
+  // fetch product data when the cart changes
+  // (the dependency array needs to include cart) as to manage async nature of fetching and states
   useEffect(() => {
     const productIds = Object.keys(cart).join(",");
     if (productIds !== "") {
@@ -40,11 +138,12 @@ export default function Cart() {
     }
   }, [cart]);
 
-  const calculateTotal = () => {
-    return products.reduce((total, product) => {
-      return total + product.price * (cart[product.id] || 0);
-    }, 0);
-  };
+  // recalculate the cart total when products or discount code changes
+  useEffect(() => {
+    if (products.length > 0) {
+      updateCartSummary(discountCode);
+    }
+  }, [products, discountCode, cart]);
 
   return (
     <div className="min-h-screen bg-my-background-1000 text-my-text-100">
@@ -110,7 +209,7 @@ export default function Cart() {
                 : "animate-out fade-out-0"
             )}
           >
-            <div className="space-y-6 w-full lg:w-2/3">
+            <div className="space-y-6 w-full lg:w-3/5">
               {products.map((product) => {
                 const quantity = cart[product.id];
 
@@ -125,17 +224,75 @@ export default function Cart() {
                 );
               })}
             </div>
-            <div className="mt-0 bg-my-background-500 p-8 rounded-lg shadow-lg h-max w-full lg:w-1/3">
-              <p className="text-2xl font-normal mb-4 text-my-text-950">
-                Total:{" "}
+            <div className="mt-0 bg-my-background-500 p-8 rounded-lg shadow-lg h-max w-full lg:w-2/5 text-lg">
+              {/* Discount code input */}
+              <div className="flex items-center justify-between mb-8 text-my-text-950 gap-4 relative">
+                <p className="font-normal whitespace-nowrap text-base">
+                  Discount Code
+                </p>
+                <input
+                  type="text"
+                  onKeyDown={handleDiscountCodeChange}
+                  placeholder="Enter discount code"
+                  className="bg-my-background-900 text-my-text-100 px-4 py-1 rounded-full font-normal text-base uppercase
+                  placeholder:text-sm relative"
+                  maxLength={10}
+                />
+                <span className="text-sm text-my-text-800 absolute right-[5%] lg:right-[10%] top-10">
+                  (Press Enter to apply)
+                </span>
+                {/* show a tick or cross depending on whether or not the discount code is valid */}
+                {isDiscountCodeValid !== null && (
+                  <span className="absolute right-2 top-1/2 -translate-y-1/2 text-xl rounded-full [&_svg]:rounded-full [&_svg]:w-5 [&_svg]:h-5 [&_svg]:p-1 ">
+                    {isDiscountCodeValid ? (
+                      <CheckIcon className="bg-green-600" />
+                    ) : (
+                      <XMarkIcon className="bg-red-600" />
+                    )}
+                  </span>
+                )}
+              </div>
+
+              {/* subtotal */}
+              <p className="text-base mb-4 text-my-text-950 flex items-center justify-between">
+                Subtotal{" "}
                 <CountUp
                   start={0}
-                  end={calculateTotal()}
+                  end={cartSummary.subTotal}
                   preserveValue
                   prefix="$"
+                  className=""
                 />
               </p>
-              <button className="w-full bg-black text-my-text-900 px-6 py-3 rounded-full font-semibold text-xl hover:bg-[#0c0c03] transition-colors duration-300">
+
+              {/* discount */}
+              {
+                <p className="text-base mb-4 text-my-text-950 flex items-center justify-between">
+                  Discount{" "}
+                  <CountUp
+                    start={0}
+                    end={cartSummary.discount}
+                    preserveValue
+                    prefix="$"
+                    className=""
+                  />
+                </p>
+              }
+
+              {/* total */}
+              <p className="mb-4 text-my-text-950 flex items-center justify-between text-xl">
+                Total{" "}
+                <CountUp
+                  start={0}
+                  end={cartSummary.total}
+                  preserveValue
+                  prefix="$"
+                  className=""
+                />
+              </p>
+
+              {/* proceed to checkout button */}
+              <button className="w-full bg-black text-my-text-900 px-6 py-3 rounded-full font-semibold text-lg hover:bg-[#0c0c03] transition-colors duration-300">
                 Proceed to Checkout
               </button>
             </div>
